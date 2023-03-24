db56ec9c1348a7eb4d2850bb0f612d825501c629
luckyjek <thxjek@gmail.com>
2023-03-24 14:53:32 +0900

    Fix : 임시적 스타일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
+// import styles from '@/styles/Home.module.css';
 import Seo from '@/components/Seo';
 import InvitaionPhrasesLayout from './layout';
 
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Seo title="Home" />
-      <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
+      {/* <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority /> */}
       <h1 className={inter.className}>Home</h1>
       {/* 청첩장 문구 */}
       <InvitaionPhrasesLayout />
