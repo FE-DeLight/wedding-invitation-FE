230e8827a6b211c046e37d1eaf4e61d1f3ca993b
kyongah <kyongah@dfluid.kr>
2023-03-29 22:51:25 +0900

    Feat : 로그인 작업중

--- src/components/Login/LoginNaver.tsx
+++ src/components/Login/LoginNaver.tsx
@@ -21,7 +21,7 @@ export default function LoginNaver() {
   const initializeNaverLogin = () => {
     const naverLogin = new window.naver.LoginWithNaverId({
       clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-      callbackUrl: 'http://localhost:3000/mypage',
+      callbackUrl: 'http://localhost:3000',
       isPopup: false,
       loginButton: { color: 'green', type: 3, height: '45' },
     });
