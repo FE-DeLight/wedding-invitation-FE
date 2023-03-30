4224ca8baac88997d632d508a6c9509b93434730
kyongah <kyongah@dfluid.kr>
2023-03-30 13:27:57 +0900

    Fix: 네이버 sdk 에러 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <Script
-        type="text/javascript"
-        src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js"
-        charSet="utf-8"
-      />
+      <Head>
+        <script async src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
