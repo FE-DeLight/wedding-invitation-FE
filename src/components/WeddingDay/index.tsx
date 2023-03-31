ad01fc4a11083c645e09a2462a1108fe8a40bbb1
kyongah <kyongah@dfluid.kr>
2023-03-31 11:25:57 +0900

    Feat: 예식일 작업중

--- src/components/WeddingDay/index.tsx
+++ src/components/WeddingDay/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import * as G from '@/styles/globals';
-import * as S from '@/components/WeddingDay/style';
-// import { ko } from 'date-fns/esm/locale';
+import styled from '@emotion/styled';
 import { getYear, getMonth, getDate, getDay } from 'date-fns';
+
 import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import ko from 'date-fns/locale/ko';
+
+registerLocale('ko', ko);
+
+const CustomDatePicker = styled(DatePicker)`
+  width: 100%;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 0px 20px;
+  background-color: #f5f5f5;
+  border: 1px solid #777;
+  font-family: 'Noto Sans KR', sans-serif;
+  color: grey;
+  font-size: 14px;
+  flex: 1 1 0;
+`;
 
 export default function WeddingDay({ startDate, setStartDate, setSelectedDate }: any) {
   function selectTime() {
@@ -28,10 +45,10 @@ export default function WeddingDay({ startDate, setStartDate, setSelectedDate }:
     <G.Row>
       <G.ColTitle>날짜</G.ColTitle>
       <G.ColContent>
-        <S.CustomDatePicker
+        <CustomDatePicker
           selected={startDate}
           popperPlacement="bottom-start"
-          // locale={ko}
+          locale="ko"
           minDate={new Date()}
           onChange={handleDateChange}
           peekNextMonth
@@ -42,6 +59,7 @@ export default function WeddingDay({ startDate, setStartDate, setSelectedDate }:
           timeFormat="HH:mm"
           timeIntervals={10}
           dateFormat="yyyy년 MM월 d일, aa h시 mm분"
+          placeholderText="클릭하여 날짜를 변경하세요."
         />
       </G.ColContent>
     </G.Row>
