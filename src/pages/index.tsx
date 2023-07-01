782cad0612252302e93700e2bd1535e570974a4e
blaze <blaze@kakaocorp.com>
2023-07-01 10:58:59 +0900

    Update. 네이버 토큰 존재하면 메인페이지로 이동, 없다면 로그인 페이지로 이동

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 import Seo from '@/components/Seo';
 import { useRouter } from 'next/router';
+import Auth from '@/utils/auth';
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/login');
+    if (!Auth.checkToken()) {
+      router.push('/login');
+    }
   }, [router]);
 
   return <Seo title="Home" />;
