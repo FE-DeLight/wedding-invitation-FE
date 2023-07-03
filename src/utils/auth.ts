4ac50111a9bcaa737ecd3e7423de4a61fc1f4414
blaze <blaze@kakaocorp.com>
2023-07-03 23:46:06 +0900

    Update. 로그아웃 로직 개발

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,12 +1,16 @@
 interface AuthOptions {
   checkToken: () => boolean;
+  logout: () => void;
 }
 
 export const auth: AuthOptions = {
   checkToken: (): boolean => {
-    const token = window?.localStorage.getItem('com.naver.nid.oauth.state_token');
+    const token = window?.localStorage.getItem('access_token');
 
     return Boolean(token);
+  },
+  logout: (): void => {
+    window.localStorage.clear();
   },
 };
 
